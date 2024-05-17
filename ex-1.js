5147fa869e6bf1aa2aa2a36e51b2b6dc68d69b36
tanayod <tanayod741@gmail.com>
2024-05-17 12:44:12 +0700

    feat:ex1-2

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,12 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let index = 0; index < array.length; index++) {
+    operation(array[index]);
+  }
+}
+function Plus5000(eachIndex) {
+  return newEmployeeSalaries.push(eachIndex + 5000);
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
@@ -9,4 +15,5 @@ const newEmployeeSalaries = [];
 
 // Using `forEach` function here
 
+forEach(employeeSalaries, Plus5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,25 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let index = 0; index < array.length; index++) {
+    if (operation(array[index])) {
+      count += 1;
+    }
+  }
+  if (count >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
+}
+
+function scoreMoreThan70(eachStudentScores) {
+  if (eachStudentScores > 70) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +29,12 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result =
+  "นักเรียนห้องที่ 1 " + atLeastFive(studentScoresRoom1, scoreMoreThan70);
+let scoreRoom2Result =
+  "นักเรียนห้องที่ 2 " + atLeastFive(studentScoresRoom2, scoreMoreThan70);
+let scoreRoom3Result =
+  "นักเรียนห้องที่ 3 " + atLeastFive(studentScoresRoom3, scoreMoreThan70);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
